d45ff1eb340153324348cbbc1a9fc05fa2e1db01
Mateo Noel Rabines <39866695+mateonoel2@users.noreply.github.com>
2025-06-06 08:46:47 -0500

    imporve protectedRoute

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,26 @@
+import { type ReactNode, useEffect } from "react";
+import { useNavigate } from "react-router";
+import useToken from "../contexts/TokenContext";
+
+type ProtectedRouteProps = {
+  children: ReactNode;
+};
+
+function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const { token } = useToken();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/");
+    }
+  }, [token, navigate]);
+
+  if (!token) {
+    return null;
+  }
+
+  return <>{children}</>;
+}
+
+export default ProtectedRoute;
