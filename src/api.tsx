5a4f3c6fc49e36d8750dfe7a4bc67265cda5c000
Mateo Noel Rabines <39866695+mateonoel2@users.noreply.github.com>
2025-06-06 08:49:49 -0500

    Update backend URL configuration to use environment variable for improved flexibility

--- src/api.tsx
+++ src/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import useToken from "./contexts/TokenContext";
 
-const BACKEND_URL = "http://localhost:8080";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export function useLogin() {
   async function login(user: { email: string; password: string }) {
